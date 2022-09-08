edad9d49a8f9f605177df60364073ebcc5682687
Rémino Rem <29999+remino@users.noreply.github.com>
2022-09-08 21:41:03 +0900

    Update igframe to v1.1.0

--- Formula/igframe.rb
+++ Formula/igframe.rb
@@ -1,9 +1,9 @@
 class Igframe < Formula
   desc "Fit image into a square frame fit for Instagram"
   homepage "https://github.com/remino/igframe"
-  url "https://api.github.com/repos/remino/igframe/zipball/v1.0.0"
-  version "1.0.0"
-  sha256 "f65ddfdf5c93158eea88e27df8b214ed68dff1f7ba9e94c664dcc12f6edc4a1d"
+  url "https://api.github.com/repos/remino/igframe/zipball/v1.1.0"
+  version "1.1.0"
+  sha256 "2cdd298fc3e5646e4fa67050ce8c5737d928a840f16769ed55e242e9aabeebfd"
   license "MIT"
 
   def install
