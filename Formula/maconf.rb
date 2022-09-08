97ddb86d9509fecfac5b5f495830ffb2f8bcb50d
Rémino Rem <29999+remino@users.noreply.github.com>
2022-09-08 16:10:41 +0900

    Fix sum for maconf

--- Formula/maconf.rb
+++ Formula/maconf.rb
@@ -3,7 +3,7 @@ class Maconf < Formula
   homepage "https://github.com/remino/maconf"
   url "https://github.com/remino/maconf/archive/refs/tags/v1.1.0.tar.gz"
   version "1.1.0"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "51b8223eba3a00b3de97e96acb4cbac9ea5fcbf64110b9eec7aee967300eb2ae"
   license "MIT"
 
   def install
