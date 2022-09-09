5aac64db30e5f91facff20603da891ba0da8a560
Rémino Rem <29999+remino@users.noreply.github.com>
2022-09-10 02:06:21 +0900

    Update unidir to 1.0.2

--- Formula/unidir.rb
+++ Formula/unidir.rb
@@ -1,9 +1,9 @@
 class Unidir < Formula
   desc "Unify the content of multiple directories into one using symlinks"
   homepage "https://github.com/remino/unidir"
-  url "https://api.github.com/repos/remino/unidir/zipball/v1.0.1"
-  version "1.0.1"
-  sha256 "217d52ef4437874745ad5efc62f542276d50cedf1f1fc8dd397ed31fd86e831c"
+  url "https://api.github.com/repos/remino/unidir/zipball/v1.0.2"
+  version "1.0.2"
+  sha256 "603ae8e56b1ae21bd2f3d16f88ef4b56cbc0467b76340d628ea42ea2554d3142"
   license "ISC"
 
   def install
