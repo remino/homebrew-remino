4d27a05565bb415bdf80e1b1edef4ab0f57e3bd4
Rémino Rem <29999+remino@users.noreply.github.com>
2022-09-10 00:01:12 +0900

    Fix typo in hashtagalong formula

--- Formula/hashtagalong.rb
+++ Formula/hashtagalong.rb
@@ -1,16 +1,16 @@
-class Hashtagalog < Formula
+class Hashtagalong < Formula
   desc "Library of hashtag helpers"
-  homepage "https://github.com/remino/hashtagalog"
-  url "https://api.github.com/repos/remino/hashtagalog/zipball/v0.1.0"
+  homepage "https://github.com/remino/hashtagalong"
+  url "https://api.github.com/repos/remino/hashtagalong/zipball/v0.1.0"
   version "1.0.0"
   sha256 "66af9c047daf3c5370a436ff04f2fcac9fb0c44ba39567f9e2429e628fd5c7a8"
   license "ISC"
 
   def install
-    bin.install "./hashtagalog"
+    bin.install "./hashtagalong"
   end
 
   test do
-    assert_equal "#a #b", shell_output("./hashtagalog -f '#b a'").strip
+    assert_equal "#a #b", shell_output("./hashtagalong -f '#b a'").strip
   end
 end
