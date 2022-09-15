bc924d6f8929ea2f3d462746f1e6303cba2063d0
Rémino Rem <29999+remino@users.noreply.github.com>
2022-09-15 12:38:52 +0900

    Update mkdldir to 1.1.0

--- Formula/mkdldir.rb
+++ Formula/mkdldir.rb
@@ -1,9 +1,9 @@
 class Mkdldir < Formula
   desc "Create password-protected download directory for Apache"
   homepage "https://github.com/remino/mkdldir"
-  url "https://api.github.com/repos/remino/mkdldir/zipball/v1.0.0"
-  version "1.0.0"
-  sha256 "371132a4af608758e2208106c478de3216a49403b6daf30ff3068d872b3292ca"
+  url "https://api.github.com/repos/remino/mkdldir/zipball/v1.1.0"
+  version "1.1.0"
+  sha256 "df5deab9183edd22fc63803299fa07af0357a8853429c5f065a5dc677ca15087"
   license "MIT"
 
   def install
