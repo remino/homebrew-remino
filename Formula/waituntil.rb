3fc9de1d84a9bb14dcbe6fb7625727e8757db1bf
Rémino Rem <29999+remino@users.noreply.github.com>
2022-09-22 21:11:53 +0900

    Update waituntil to 1.1.0

--- Formula/waituntil.rb
+++ Formula/waituntil.rb
@@ -1,9 +1,9 @@
 class Waituntil < Formula
   desc "Make new shell script executable file from template"
   homepage "https://github.com/remino/waituntil"
-  url "https://api.github.com/repos/remino/waituntil/zipball/v1.0.0"
-  version "1.0.0"
-  sha256 "ad75bffacf9ff03ceef53489f3459cb8b047405898af39c20a1d3dd876142051"
+  url "https://api.github.com/repos/remino/waituntil/zipball/v1.1.0"
+  version "1.1.0"
+  sha256 "07d9046a670cb714a1729b197f4f6e3a959ed25d2d66bcf72eca135654dfa005"
   license "MIT"
 
   def install
