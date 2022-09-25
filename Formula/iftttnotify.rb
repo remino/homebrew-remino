a20cec57aeadb50d39ec8ec2f2ec9ca9bb85e747
Rémino Rem <29999+remino@users.noreply.github.com>
2022-09-25 16:50:07 +0900

    Update iftttnotify to 1.0.1

--- Formula/iftttnotify.rb
+++ Formula/iftttnotify.rb
@@ -1,9 +1,9 @@
 class Iftttnotify < Formula
   desc "Send notification to IFTTT Webhook using Web request"
   homepage "https://github.com/remino/iftttnotify"
-  url "https://api.github.com/repos/remino/iftttnotify/zipball/v1.0.0"
-  version "1.0.0"
-  sha256 "2bd17ebb108031d1df1ed4509aa35a859b196b6ddfcecb72dc1169063d2d3431"
+  url "https://api.github.com/repos/remino/iftttnotify/zipball/v1.0.1"
+  version "1.0.1"
+  sha256 "31557c7ea2fb3cb1cbe5b69e9d0a02414a44565ff028e2f6f04221d804f9f356"
   license "MIT"
 
   def install
