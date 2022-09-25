0a47b20d4232788be0aebe37528ea407ff62976a
Rémino Rem <29999+remino@users.noreply.github.com>
2022-09-25 15:48:50 +0900

    Update vidmod to 1.0.1

--- Formula/vidmod.rb
+++ Formula/vidmod.rb
@@ -1,9 +1,9 @@
 class Vidmod < Formula
   desc "Make new shell script executable file from template"
   homepage "https://github.com/remino/vidmod"
-  url "https://api.github.com/repos/remino/vidmod/zipball/v1.0.0"
-  version "1.0.0"
-  sha256 "d74055f669095508435daffef9bb7ec983a9ed45dacf9327aaa3953f8f1e3aca"
+  url "https://api.github.com/repos/remino/vidmod/zipball/v1.0.1"
+  version "1.0.1"
+  sha256 "0a85f580de09341a634403caf4df97398865c4496cf508aacb7c5333599d2f56"
   license "MIT"
 
   def install
