79acf49d9f15a3a9af61652ca243649058522170
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-05 09:49:04 +0900

    Update podsite to 1.1.0

--- podsite.rb
+++ podsite.rb
@@ -1,9 +1,9 @@
 class Podsite < Formula
 	desc "Container directory of pods to unify as a single site"
 	homepage "https://github.com/remino/podsite"
-	url "https://api.github.com/repos/remino/podsite/tarball/v1.0.4"
-	sha256 "eaeaf6645c9ccaf2222f234cdde7754158cebadc767fbaf35c3a010890a20ecf"
-	version "1.0.4"
+	url "https://api.github.com/repos/remino/podsite/tarball/v1.1.0"
+	sha256 "9dd35a89f4e51553b52f2153ffbfc10046446fd58c2a9261753ab86df4e807ea"
+	version "1.1.0"
 	license "ISC"
 
 	def install
