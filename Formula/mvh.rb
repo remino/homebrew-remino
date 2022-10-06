98f5ee71010546d4c160e5a3986e024cb95cd882
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-06 22:05:20 +0900

    Adjust description of mvh

--- Formula/mvh.rb
+++ Formula/mvh.rb
@@ -1,5 +1,5 @@
 class Mvh < Formula
-	desc "Move files and directories to current directory using mv."
+	desc "Move files and directories to current directory using mv"
 	homepage "https://github.com/remino/mvh"
 	url "https://api.github.com/repos/remino/mvh/tarball/v1.0.0"
 	sha256 "3add4cca8172484f044d86fa15df7681d0ac0b190a8f6eaf44ffa25bfb19b4e3"
