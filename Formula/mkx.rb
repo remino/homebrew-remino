5ba0976a3fff3942abfba454c54c4c0eae53a691
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-06 10:37:02 +0900

    Update mkx to 2.1.0

--- Formula/mkx.rb
+++ Formula/mkx.rb
@@ -1,16 +1,16 @@
 class Mkx < Formula
-  desc "Make new shell script executable file from template"
-  homepage "https://github.com/remino/mkx"
-  url "https://api.github.com/repos/remino/mkx/zipball/v2.0.0"
-  version "2.0.0"
-  sha256 "5e9760c1b5f96f12de4a12c3ce633aa2322f4b1c093574e57dfe72f552fcae64"
-  license "MIT"
+	desc "Make new shell script executable file from template"
+	homepage "https://github.com/remino/mkx"
+	url "https://api.github.com/repos/remino/mkx/tarball/v2.1.0"
+	sha256 "16c1fa65d3232ad27f358b3c07ed11e0baae80b14e08161f8c2c52444513c6c3"
+	version "2.1.0"
+	license "ISC"
 
-  def install
-    bin.install "./mkx"
-  end
+	def install
+		bin.install "./mkx"
+	end
 
-  test do
-    system "./mkx", "-v"
-  end
+	test do
+		system "./mkx", "-v"
+	end
 end
