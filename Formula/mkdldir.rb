909bcf21f2d39ce25fe45720b6ae1311f03264e4
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-12 22:58:33 +0900

    Update mkdldir to 1.3.0

--- Formula/mkdldir.rb
+++ Formula/mkdldir.rb
@@ -1,9 +1,9 @@
 class Mkdldir < Formula
 	desc "Generate download directory for Apache HTTP Server"
 	homepage "https://github.com/remino/mkdldir"
-	url "https://api.github.com/repos/remino/mkdldir/tarball/v1.2.0"
-	sha256 "f6a8e8c0f4e5bceb374fba50683f7e6044f5373b7b6928ff6a26223b8243e782"
-	version "1.2.0"
+	url "https://api.github.com/repos/remino/mkdldir/tarball/v1.3.0"
+	sha256 "bbf8e09b789dbd4ef776093d149affee3dddbc3caa75e506b36cd60670c8834e"
+	version "1.3.0"
 	license "ISC"
 
 	def install
