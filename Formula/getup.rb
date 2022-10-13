d14c81df46586eff3d13256b102fbb2ed56b5910
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-13 22:31:46 +0900

    Add getup formula

---
+++ Formula/getup.rb
@@ -0,0 +1,48 @@
+class Getup < Formula
+	desc "Get up and run some things"
+	homepage "https://github.com/remino/getup"
+	url ""
+	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+	version ""
+	license "ISC"
+
+	def install
+		bin.install "./getup"
+	end
+
+	test do
+		system "./getup", "-v"
+	end
+end
+class Getup < Formula
+	desc "Get up and run some things"
+	homepage "https://github.com/remino/getup"
+	url ""
+	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+	version ""
+	license "ISC"
+
+	def install
+		bin.install "./getup"
+	end
+
+	test do
+		system "./getup", "-v"
+	end
+end
+class Getup < Formula
+	desc "Get up and run some things"
+	homepage "https://github.com/remino/getup"
+	url "https://api.github.com/repos/remino/getup/tarball/v1.0.0"
+	sha256 "b00b8a6c19f58ab70b6e7e0f11266a5225ab8a8c01ab386014e88cd293149246"
+	version "1.0.0"
+	license "ISC"
+
+	def install
+		bin.install "./getup"
+	end
+
+	test do
+		system "./getup", "-v"
+	end
+end
