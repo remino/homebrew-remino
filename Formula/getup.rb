0ab4a71b85c2090805f4d4541bd9eb6ec795e492
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-20 23:28:18 +0900

    Update getup to 1.0.1

--- Formula/getup.rb
+++ Formula/getup.rb
@@ -1,41 +1,9 @@
 class Getup < Formula
 	desc "Get up and run some things"
 	homepage "https://github.com/remino/getup"
-	url ""
-	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-	version ""
-	license "ISC"
-
-	def install
-		bin.install "./getup"
-	end
-
-	test do
-		system "./getup", "-v"
-	end
-end
-class Getup < Formula
-	desc "Get up and run some things"
-	homepage "https://github.com/remino/getup"
-	url ""
-	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-	version ""
-	license "ISC"
-
-	def install
-		bin.install "./getup"
-	end
-
-	test do
-		system "./getup", "-v"
-	end
-end
-class Getup < Formula
-	desc "Get up and run some things"
-	homepage "https://github.com/remino/getup"
-	url "https://api.github.com/repos/remino/getup/tarball/v1.0.0"
-	sha256 "b00b8a6c19f58ab70b6e7e0f11266a5225ab8a8c01ab386014e88cd293149246"
-	version "1.0.0"
+	url "https://api.github.com/repos/remino/getup/tarball/v1.0.1"
+	sha256 "30218f7c1192908211af81f7a08735b3a2137a20c3b406e301775b0e67c8e60b"
+	version "1.0.1"
 	license "ISC"
 
 	def install
