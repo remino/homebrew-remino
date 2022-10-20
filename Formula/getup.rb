6a479591d747a8f176fb5a0f2e5e54ec781c1509
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-20 23:36:27 +0900

    Update getup to 1.1.0

--- Formula/getup.rb
+++ Formula/getup.rb
@@ -1,9 +1,9 @@
 class Getup < Formula
 	desc "Get up and run some things"
 	homepage "https://github.com/remino/getup"
-	url "https://api.github.com/repos/remino/getup/tarball/v1.0.1"
-	sha256 "30218f7c1192908211af81f7a08735b3a2137a20c3b406e301775b0e67c8e60b"
-	version "1.0.1"
+	url "https://api.github.com/repos/remino/getup/tarball/v1.1.0"
+	sha256 "0552e9a386df6d421a308d0742ec4a870ee7c490afe9483cb694e128c7b6b6c1"
+	version "1.1.0"
 	license "ISC"
 
 	def install
