f05090deeca6d5f95852e32b660d86a229ded2d4
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-25 21:46:06 +0900

    Update mkwebalbum to 1.3.0

--- Formula/mkwebalbum.rb
+++ Formula/mkwebalbum.rb
@@ -1,9 +1,9 @@
 class Mkwebalbum < Formula
 	desc "Generate album Web page with previews from a directory of photos"
 	homepage "https://github.com/remino/mkwebalbum"
-	url "https://api.github.com/repos/remino/mkwebalbum/tarball/v1.2.0"
-	sha256 "2af0fe5e3f0fde7162dc8e1f6c51cd490ce262da0b502db6d70548823ada7455"
-	version "1.2.0"
+	url "https://api.github.com/repos/remino/mkwebalbum/tarball/v1.3.0"
+	sha256 "356acd10ef9724abcad95f49eb659800dc04074a3db16cc993a30642481df267"
+	version "1.3.0"
 	license "ISC"
 
 	def install
