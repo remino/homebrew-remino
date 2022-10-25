54d9728f7f732b2ad3d4e76dd46dfebb63ab3b05
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-25 13:01:12 +0900

    Update dldirs to 1.1.0

--- Formula/dldirs.rb
+++ Formula/dldirs.rb
@@ -1,9 +1,9 @@
 class Dldirs < Formula
 	desc "Manage download directories"
 	homepage "https://github.com/remino/dldirs"
-	url "https://api.github.com/repos/remino/dldirs/tarball/v1.0.0"
-	sha256 "21174d98c5174d9398823b5cdf584306a37ce3630d2b0dbc7f8789ea6bf6a41b"
-	version "1.0.0"
+	url "https://api.github.com/repos/remino/dldirs/tarball/v1.1.0"
+	sha256 "827e98db61b23b518de221ee6dccc6d604b2d0c68f73dd7a72e098b7a443c09a"
+	version "1.1.0"
 	license "ISC"
 
 	def install
