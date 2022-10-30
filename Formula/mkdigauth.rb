3e77a787c3515c69d248dc6f15b4695202cfb4a9
Rémino Rem <29999+remino@users.noreply.github.com>
2022-10-30 20:20:41 +0900

    Update mkdigauth to 1.1.1

--- Formula/mkdigauth.rb
+++ Formula/mkdigauth.rb
@@ -1,9 +1,9 @@
 class Mkdigauth < Formula
 	desc "Create digest auth .htdigest file for Apache HTTPd with related .htaccess file"
 	homepage "https://github.com/remino/mkdigauth"
-	url "https://api.github.com/repos/remino/mkdigauth/tarball/v1.1.0"
-	sha256 "bbdf05b3896b15fcb54c67e192621dfc03dde078db1802e26d5ffc924def75cd"
-	version "1.1.0"
+	url "https://api.github.com/repos/remino/mkdigauth/tarball/v1.1.1"
+	sha256 "73d348678f6b616d1d4a1f9e7145563cd1e9b79f2a6b1e48fda2b754a80f6ad3"
+	version "1.1.1"
 	license "ISC"
 
 	def install
