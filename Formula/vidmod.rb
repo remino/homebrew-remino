6fc9639b29117e284857bcc85d6275ad103cafcd
Rémino Rem <29999+remino@users.noreply.github.com>
2022-11-18 18:49:51 +0900

    Add vidmod formula

--- Formula/vidmod.rb
+++ Formula/vidmod.rb
@@ -1,16 +1,18 @@
 class Vidmod < Formula
-  desc "Do rapid changes to video files using ffmpeg or other tools"
-  homepage "https://github.com/remino/vidmod"
-  url "https://api.github.com/repos/remino/vidmod/zipball/v1.0.2"
-  version "1.0.2"
-  sha256 "38668a606ac71f334af35e50c16a8684c313a4c95518ffb7ebbfe4ff1c6a87cc"
-  license "MIT"
+	desc "Do rapid changes to video files using ffmpeg or other tools"
+	homepage "https://github.com/remino/vidmod"
+	url "https://api.github.com/repos/remino/vidmod/tarball/v1.1.0"
+	sha256 "1c8c1dad3cb057e87c75f2a14b57e7e755bb7aa08a524901cb527e3beea23a93"
+	version "1.1.0"
+	license "ISC"
 
-  def install
-    bin.install "./vidmod"
-  end
+	depends_on "ffmpeg"
 
-  test do
-    system "./vidmod", "-v"
-  end
+	def install
+		bin.install "./vidmod"
+	end
+
+	test do
+		system "./vidmod", "-v"
+	end
 end
