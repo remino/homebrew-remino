80788c16ebe4f82bc868390d86519b252e4e6b7d
Rémino Rem <29999+remino@users.noreply.github.com>
2022-12-12 22:06:58 +0900

    Update jeksite to 1.1.1:
    Also indent formula using tabs instead of spaces.

--- Formula/jeksite.rb
+++ Formula/jeksite.rb
@@ -1,16 +1,16 @@
 class Jeksite < Formula
-  desc "Plain and simple Jekyll site template ideal for GitHub Pages"
-  homepage "https://github.com/remino/jeksite"
-  url "https://api.github.com/repos/remino/jeksite/zipball/v1.1.0"
-  version "1.1.0"
-  sha256 "238b006daedda8dd2715525814c0c4bcda54d4cf4c70321c0e56c415ce27a9af"
-  license "ISC"
+	desc "Plain and simple Jekyll site template ideal for GitHub Pages"
+	homepage "https://github.com/remino/jeksite"
+	url "https://api.github.com/repos/remino/jeksite/tarball/v1.1.1"
+	sha256 "8fac3c383dfc5e5d10ba853b43f884576a0f7bdb36be1b2be3b7d329da45b5d7"
+	version "1.1.1"
+	license "ISC"
 
-  def install
-    bin.install "./jeksite"
-  end
+	def install
+		bin.install "./jeksite"
+	end
 
-  test do
-    system "./jeksite", "-v"
-  end
+	test do
+		system "./jeksite", "-v"
+	end
 end
