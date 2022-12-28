e7cd39d556c131807d50bcfd9fc321e51a25b6a3
Rémino Rem <29999+remino@users.noreply.github.com>
2022-12-28 19:57:48 +0900

    Update podsite to v1.1.2, now depends on unidir

--- Formula/podsite.rb
+++ Formula/podsite.rb
@@ -1,10 +1,12 @@
 class Podsite < Formula
 	desc "Container directory of pods to unify as a single site"
 	homepage "https://github.com/remino/podsite"
-	url "https://api.github.com/repos/remino/podsite/tarball/v1.1.1"
-	sha256 "16c6728ce353736714a857ee4e5dea37a4e8b29995f1ab5c60022f5a5ffa6f09"
-	version "1.1.1"
+	url "https://api.github.com/repos/remino/podsite/tarball/v1.1.2"
+	sha256 "2a797c18d6760d9bbdf38fbb698dd3e61c8ffa087ce5d5fa93983230bec9db25"
+	version "1.1.2"
 	license "ISC"
+
+	depends_on "unidir"
 
 	def install
 		bin.install "./podsite"
