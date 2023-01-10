e1b26134be12c09c833ba5b8b249a37f4d5b8f02
Rémino Rem <29999+remino@users.noreply.github.com>
2023-01-10 15:51:10 +0900

    Update mansite to 2.0.3

--- Formula/mansite.rb
+++ Formula/mansite.rb
@@ -1,9 +1,9 @@
 class Mansite < Formula
 	desc "Plain and simple Middleman site template"
 	homepage "https://github.com/remino/mansite"
-	url "https://api.github.com/repos/remino/mansite/tarball/v2.0.2"
-	sha256 "2a4cdb54799ce7e3598e2ed8acc070ad4cd8b26924d6797e92a41d6e7e744a60"
-	version "2.0.2"
+	url "https://api.github.com/repos/remino/mansite/tarball/v2.0.3"
+	sha256 "a871bea86d906bff20427d7f5542b51a378393e69f75633321d693f2751145e5"
+	version "2.0.3"
 	license "ISC"
 
 	def install
