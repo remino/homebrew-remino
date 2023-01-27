947782356b48be02747d173fef6951dfe777de74
Rémino Rem <29999+remino@users.noreply.github.com>
2023-01-27 18:13:15 +0900

    Update addwatermark to 1.1.0

--- Formula/addwatermark.rb
+++ Formula/addwatermark.rb
@@ -1,9 +1,9 @@
 class Addwatermark < Formula
 	desc "Apply a watermark to an image file using ImageMagick"
 	homepage "https://github.com/remino/addwatermark"
-	url "https://api.github.com/repos/remino/addwatermark/tarball/v1.0.0"
-	sha256 "efd6e81b40a12d8f137f0a8bbedf156e74d8c43b58fe161b965271539e64c9cf"
-	version "1.0.0"
+	url "https://api.github.com/repos/remino/addwatermark/tarball/v1.1.0"
+	sha256 "83bf34e0a83aabe3cdfaabe8bf023b402d0b5f3ef96d8a9a17512e7a82d0c0c5"
+	version "1.1.0"
 	license "ISC"
 
 	def install
