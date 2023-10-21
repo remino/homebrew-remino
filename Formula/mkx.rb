8d920ffe2b516c83bec150f62f4b33deb310e2ee
Rémino Rem <29999+remino@users.noreply.github.com>
2023-10-21 10:21:52 +0900

    Update mkx to version 2.3.0

--- Formula/mkx.rb
+++ Formula/mkx.rb
@@ -1,9 +1,9 @@
 class Mkx < Formula
 	desc "Make new shell script executable file from template"
 	homepage "https://github.com/remino/mkx"
-	url "https://api.github.com/repos/remino/mkx/tarball/v2.2.0"
-	sha256 "723a420422e099a7905c7c2ea391454c61f1eb81b4ceeb879ba0e2eb8149a14a"
-	version "2.2.0"
+	url "https://api.github.com/repos/remino/mkx/tarball/v2.3.0"
+	sha256 "bb4e18f47202f9eafa51e8ff07fd87a8da7d5ea1653b148efc044600ca5d6ae3"
+	version "2.3.0"
 	license "ISC"
 
 	def install
