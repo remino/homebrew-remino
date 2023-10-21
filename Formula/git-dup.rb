a9c57f4518bf964f62a1ef4491ac6ae9cad07710
Rémino Rem <29999+remino@users.noreply.github.com>
2023-10-21 10:18:13 +0900

    Update git-dup formula to version 1.1.0

--- Formula/git-dup.rb
+++ Formula/git-dup.rb
@@ -1,9 +1,9 @@
 class GitDup < Formula
 	desc "Copy a local git repository from one location to another."
 	homepage "https://github.com/remino/git-dup"
-	url "https://api.github.com/repos/remino/git-dup/tarball/v1.0.0"
-	sha256 "30a8330bcd2f3cd064975923e5125977530e1390820408d5dd9cff83f5e35121"
-	version "1.0.0"
+	url "https://api.github.com/repos/remino/git-dup/tarball/v1.1.0"
+	sha256 "1a1156c83b8cc95abf2084877ca6b632ef6df0adf0bcc49c332cfd439c6c4cfa"
+	version "1.1.0"
 	license "ISC"
 
 	def install
