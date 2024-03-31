3fb9e7b3e3b240a451e2b82a26c35c00036ad0db
Rémino Rem <29999+remino@users.noreply.github.com>
2024-03-31 16:14:53 +0900

    Update vidmod to 1.2.0

--- Formula/vidmod.rb
+++ Formula/vidmod.rb
@@ -1,9 +1,9 @@
 class Vidmod < Formula
 	desc "Do rapid changes to video files using ffmpeg or other tools"
 	homepage "https://github.com/remino/vidmod"
-	url "https://api.github.com/repos/remino/vidmod/tarball/v1.1.0"
-	sha256 "1c8c1dad3cb057e87c75f2a14b57e7e755bb7aa08a524901cb527e3beea23a93"
-	version "1.1.0"
+	url "https://api.github.com/repos/remino/vidmod/tarball/v1.2.0"
+	sha256 "063e046e1a901af46c5f7ef42deba3f2818a4a594765aa88f4f85fcd70f4ebfb"
+	version "1.2.0"
 	license "ISC"
 
 	depends_on "ffmpeg"
