dffa5e3f2f2efb380028947a0ee6534f4abc1eb4
Rémino Rem <29999+remino@users.noreply.github.com>
2024-08-16 17:49:17 +0900

    Update sere to 2.2.0

--- Formula/sere.rb
+++ Formula/sere.rb
@@ -1,9 +1,9 @@
 class Sere < Formula
 	desc "Search and replace"
 	homepage "https://github.com/remino/sere"
-	url "https://api.github.com/repos/remino/sere/tarball/v2.1.0"
-	sha256 "05f2551d68cbacf2b0ef7699c2d6b0bdf285488b9e9aa081710133b115de81d9"
-	version "2.1.0"
+	url "https://api.github.com/repos/remino/sere/tarball/v2.2.0"
+	sha256 "894b39783d8273fe685f8a01497d158c3daf05b7ff3c53ae1fcc085e934393c1"
+	version "2.2.0"
 	license "ISC"
 
 	def install
