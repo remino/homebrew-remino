b94ff2cf951e76b3308b081852f78f0c7d58adc7
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-04 21:47:44 +0900

    Fix install copy in mkbrewrel setup

--- Formula/mkbrewrel.rb
+++ Formula/mkbrewrel.rb
@@ -3,14 +3,14 @@ class Mkbrewrel < Formula
 	homepage "https://github.com/remino/mkbrewrel"
 	url "https://api.github.com/repos/remino/mkbrewrel/tarball/v2.0.3"
 	sha256 "8eab88ea740fb6aa47735ea260b683e1c0dccbf1d3559ab1c3c4055609595072"
-	version "2.0.3_2"
+	version "2.0.3_3"
 	license "ISC"
 
 	def install
 		libexec.install "mkbrewrel"
 		lib.mkpath
 		(lib/"mkbrewrel").mkpath
-		(lib/"mkbrewrel").install Dir["lib/*"], "lib/mkbrewrel"
+		(lib/"mkbrewrel").install Dir["lib/*"]
 
 		man1.install "man/mkbrewrel.1"
 
