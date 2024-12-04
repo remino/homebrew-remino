adb5d105361636c397eea4a0ed79d75b49a18abc
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-04 21:45:44 +0900

    Adjust position of mo lib for mkbrewrel

--- Formula/mkbrewrel.rb
+++ Formula/mkbrewrel.rb
@@ -3,17 +3,19 @@ class Mkbrewrel < Formula
 	homepage "https://github.com/remino/mkbrewrel"
 	url "https://api.github.com/repos/remino/mkbrewrel/tarball/v2.0.3"
 	sha256 "8eab88ea740fb6aa47735ea260b683e1c0dccbf1d3559ab1c3c4055609595072"
-	version "2.0.3"
+	version "2.0.3_1"
 	license "ISC"
 
 	def install
 		libexec.install "mkbrewrel"
-		lib.install Dir["lib/*"]
+		lib.mkpath
+		(lib/"mkbrewrel").install Dir["lib/*"], "lib/mkbrewrel"
+
 		man1.install "man/mkbrewrel.1"
 
 		(bin/"mkbrewrel").write <<~EOS
 			#!/usr/bin/env bash
-			export MKBREWREL_LIB_DIR="#{lib}"
+			export MKBREWREL_LIB_DIR="#{lib}/mkbrewrel"
 			exec "#{libexec}/mkbrewrel" "$@"
 		EOS
 	end
