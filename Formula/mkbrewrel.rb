49d538684fcf910de508673b3d2a33883bec660a
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-04 21:17:10 +0900

    Update mkbrewrel to v2.0.1

--- Formula/mkbrewrel.rb
+++ Formula/mkbrewrel.rb
@@ -1,9 +1,9 @@
 class Mkbrewrel < Formula
 	desc "Create Brew release from GitHub release"
 	homepage "https://github.com/remino/mkbrewrel"
-	url "https://api.github.com/repos/remino/mkbrewrel/tarball/v1.1.4"
-	sha256 "b23d22e59332f23de41f2ef8f5d392ebbf34540c155b293e5b055f5cb35637ee"
-	version "1.1.4"
+	url "https://api.github.com/repos/remino/mkbrewrel/tarball/v2.0.1"
+	sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+	version "2.0.1"
 	license "ISC"
 
 	def install
