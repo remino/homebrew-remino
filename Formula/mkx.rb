4e603594dd7a5a6f3a9dc4cfc0aeea5c2f9535cd
Rémino Rem <29999+remino@users.noreply.github.com>
2024-12-08 11:13:34 +0900

    Update mkx to 3.1.0

--- Formula/mkx.rb
+++ Formula/mkx.rb
@@ -1,9 +1,9 @@
 class Mkx < Formula
 	desc "Make new shell script executable file from template"
-	version "3.0.0"
 	homepage "https://github.com/remino/mkx"
-	url "https://api.github.com/repos/remino/mkx/tarball/v#{version}"
-	sha256 "4d03da6ccb4e5a72693b4143d5fe807813ba5957364afce764aaf67d52fd945d"
+	url "https://api.github.com/repos/remino/mkx/tarball/v3.1.0"
+	sha256 "9fd4c3ef53dddeeb914f9761f1ce74fe329ce7f6d9035c908ba1f10a67f1059a"
+	version "3.1.0"
 	license "ISC"
 
 	def install
