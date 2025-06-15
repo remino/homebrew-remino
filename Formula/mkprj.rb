75344ad879b4bd1c173e406190851118a9aa0e8c
Rémino Rem <29999+remino@users.noreply.github.com>
2025-06-15 22:25:53 +0900

    Update mkprj to 2.1.0

--- Formula/mkprj.rb
+++ Formula/mkprj.rb
@@ -1,9 +1,9 @@
 class Mkprj < Formula
 	desc "Create a dated project directory"
 	homepage "https://github.com/remino/mkprj"
-	url "https://api.github.com/repos/remino/mkprj/tarball/v2.0.0"
-	sha256 "c2266e9ba47756c91774527d2814b4542c285f4a9de37ab2532239d189e658b2"
-	version "2.0.0"
+	url "https://api.github.com/repos/remino/mkprj/tarball/v2.1.0"
+	sha256 "466d6b8895121e8bf28a5fb46dd367b604403d272e7e42acd5c25f3b8c6143a7"
+	version "2.1.0"
 	license "ISC"
 
 	def install
