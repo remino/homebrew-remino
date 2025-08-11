192e454a9865ebc1ef42bdaabb1994bebca29b27
Rémino Rem <29999+remino@users.noreply.github.com>
2025-08-11 16:52:38 +0900

    Update mkx

--- Formula/mkx.rb
+++ Formula/mkx.rb
@@ -1,8 +1,8 @@
 class Mkx < Formula
   desc "A script to make scripts"
   homepage "https://github.com/remino/mkx"
-  url "https://api.github.com/repos/remino/mkx/tarball/v3.2.1"
-  sha256 "785ecfb4ed900d65360aaf4cfc9dd01d417ea09f8e9496f773436541c4851c22"
+  url "https://api.github.com/repos/remino/mkx/tarball/v3.2.2"
+  sha256 "808e60e9e736176257ee28dd33db16e34ce447eed221a13465f45154ff0b18ed"
   license "ISC"
 
   depends_on "bash"
