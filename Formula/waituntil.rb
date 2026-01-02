a2c3e64ee6ed3980c85a0236e9f64bd211365b5d
Rémino Rem <29999+remino@users.noreply.github.com>
2026-01-02 21:36:19 +0900

    Update waituntil from remutils repo

--- Formula/waituntil.rb
+++ Formula/waituntil.rb
@@ -1,18 +1,29 @@
+# vim: set ft=ruby :
 class Waituntil < Formula
-  desc "Make new shell script executable file from template"
-  homepage "https://github.com/remino/waituntil"
-  url "https://api.github.com/repos/remino/waituntil/zipball/v1.1.0"
+  desc "Wait until a specified time and optionally execute a command."
   version "1.1.0"
-  sha256 "07d9046a670cb714a1729b197f4f6e3a959ed25d2d66bcf72eca135654dfa005"
-  license "MIT"
+  url "https://github.com/remino/remutils/releases/download/waituntil@1.1.0/waituntil@1.1.0.tar.gz"
+  sha256 "f9f60667dc69eafe93861c0fd7e32830d29b050202b0167d88866eedb2baaab3"
+  license "ISC"
+  homepage "https://github.com/remino/remutils"
+  revision 1
+
+  depends_on "bash"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
   def install
-    bin.install "./runat"
-    bin.install "./waituntil"
+    bin.install "waituntil"
+    bin.install "runat"
+    man1.install "man/waituntil.1"
+    man1.install "man/runat.1"
   end
 
   test do
-    system "./runat", "-v"
-    system "./waituntil", "-v"
+    system "#{bin}/waituntil", "-v"
+    system "#{bin}/runat", "-v"
   end
 end
