aa2f63b8840f978f5cec7f31cd0396bb9f74651b
Rémino Rem <29999+remino@users.noreply.github.com>
2026-01-02 22:25:28 +0900

    Update liverename to 1.0.1

--- Formula/liverename.rb
+++ Formula/liverename.rb
@@ -1,9 +1,9 @@
 # vim: set ft=ruby :
 class Liverename < Formula
   desc "Watches a file with watchexec and renames it with a timestamp when it is created."
-  version "1.0.0"
-  url "https://github.com/remino/remutils/releases/download/liverename@1.0.0/liverename@1.0.0.tar.gz"
-  sha256 "f58ab57818a953d38ffdbc9bdbf1c58ed241d88b9557bbd245dd38d98aa976ab"
+  version "1.0.1"
+  url "https://github.com/remino/remutils/releases/download/liverename@1.0.1/liverename@1.0.1.tar.gz"
+  sha256 "5712846c277f5c0a6ca7e471cb7fd047b26c365c33350f984be94fa56647ed73"
   license "ISC"
   homepage "https://github.com/remino/remutils"
 
