a454d27bc88f6636403c5a4719af9fb5c20aeaf0
Rémino Rem <29999+remino@users.noreply.github.com>
2026-01-02 19:04:00 +0900

    Add mkiso

---
+++ Formula/mkiso.rb
@@ -0,0 +1,32 @@
+# vim: set ft=ruby :
+class Hello < Formula
+  desc "Wrapper for hdiutil to make an ISO volume file out of a directory."
+  version "1.0.0"
+  url "https://github.com/remino/remutils/releases/download/mkiso@1.0.0/mkiso@1.0.0.tar.gz"
+  sha256 "af06e7a7518efc43ce3406b9eca2af2e97c8ca850f2d5025a2997859c74457d1"
+  license "ISC"
+  homepage "https://github.com/remino/remutils"
+
+  depends_on "bash"
+  uses_from_macos "hdiutil"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  def install
+    bin.install "mkiso"
+  end
+
+  test do
+    (testpath / "input").mkpath
+    (testpath / "input/hello.txt").write("hello")
+
+    system bin / "mkiso", "input", "out.iso"
+
+    assert_predicate testpath / "out.iso", :exist?
+    assert_predicate testpath / "out.iso", :file?
+    assert (testpath / "out.iso").size > 0
+  end
+end
