b11527e39da85ccfb8217bdda779017f345f0868
Rémino Rem <29999+remino@users.noreply.github.com>
2026-01-02 17:10:42 +0900

    Add hello formula

---
+++ Formula/hello.rb
@@ -0,0 +1,25 @@
+# vim: set ft=ruby :
+class Hello < Formula
+  desc "Just saying hello!"
+  version "1.0.1"
+  url "https://github.com/remino/remutils/releases/download/hello@1.0.1/hello@1.0.1.tar.gz"
+  sha256 "bb286d8841efc934922a38cdba964421c5aaf27abda20ce09ca11a9082dcf28f"
+  license "ISC"
+  homepage "https://github.com/remino/remutils"
+
+  depends_on "bash"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  def install
+    bin.install ""
+  end
+
+  test do
+    out = shell_output("#{bin}/")
+    assert_match "hello", out
+  end
+end
