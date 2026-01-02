17ca55df32241d57b03a0ed2cb3ba34d9536fa26
Rémino Rem <29999+remino@users.noreply.github.com>
2026-01-02 22:18:20 +0900

    Update waituntil to 1.1.1

--- Formula/waituntil.rb
+++ Formula/waituntil.rb
@@ -1,12 +1,11 @@
 # vim: set ft=ruby :
 class Waituntil < Formula
   desc "Wait until a specified time and optionally execute a command."
-  version "1.1.0"
-  url "https://github.com/remino/remutils/releases/download/waituntil@1.1.0/waituntil@1.1.0.tar.gz"
-  sha256 "f9f60667dc69eafe93861c0fd7e32830d29b050202b0167d88866eedb2baaab3"
+  version "1.1.1"
+  url "https://github.com/remino/remutils/releases/download/waituntil@1.1.1/waituntil@1.1.1.tar.gz"
+  sha256 "bbada3ca08b2bf3480d30280048a68093728111e857e576b11a1fc3e814de3b8"
   license "ISC"
   homepage "https://github.com/remino/remutils"
-  revision 1
 
   depends_on "bash"
 
