c86783827cb3265ff0f219d67fc2e7459e29dcf6
Rémino Rem <29999+remino@users.noreply.github.com>
2026-01-03 12:55:16 +0900

    Revise rsdeploy, get from remutils

--- Formula/rsdeploy.rb
+++ Formula/rsdeploy.rb
@@ -1,16 +1,26 @@
+# vim: set ft=ruby :
 class Rsdeploy < Formula
-	desc ""
-	homepage "https://github.com/remino/rsdeploy"
-	url "https://api.github.com/repos/remino/rsdeploy/tarball/v1.1.0"
-	sha256 "030277cb7e215f8a74365d5a1af3d23566d31842075d652ba441834c0afacf51"
-	version "1.1.0"
-	license "ISC"
+  desc "Deploy directory to remote server by using rsync."
+  version "1.1.0"
+  url "https://github.com/remino/remutils/releases/download/rsdeploy@1.1.0/rsdeploy@1.1.0.tar.gz"
+  sha256 "ca602bda3408746e57809c8dbbcc1f03630054889fbf14824a318ceadbc0d798"
+  license "ISC"
+  homepage "https://github.com/remino/remutils"
+  revision 1
 
-	def install
-		bin.install "./rsdeploy"
-	end
+  depends_on "bash"
 
-	test do
-		system "./rsdeploy", "-v"
-	end
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  def install
+    bin.install "rsdeploy"
+  end
+
+  test do
+    out = shell_output("#{bin}/rsdeploy -v")
+    assert_match version.to_s, out
+  end
 end
