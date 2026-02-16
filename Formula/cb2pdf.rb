2a785d00d5aea4463b08c47f83efbac0dd3b437f
Rémino Rem <29999+remino@users.noreply.github.com>
2026-02-16 18:27:38 +0900

    Add cb2pdf

---
+++ Formula/cb2pdf.rb
@@ -0,0 +1,27 @@
+# vim: set ft=ruby :
+class Cb2pdf < Formula
+  desc "Convert .cbz and .cbr comic archives to PDF."
+  version "1.0.0"
+  url "https://github.com/remino/remutils/releases/download/cb2pdf@1.0.0/cb2pdf@1.0.0.tar.gz"
+  sha256 "a40e261caeb4f67d91f248a200be9e5d05f5db7e17840b7a166d42018a653f8c"
+  license "ISC"
+  homepage "https://github.com/remino/remutils"
+
+  depends_on "bash"
+  depends_on "7zip"
+  depends_on "img2pdf"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  def install
+    bin.install "cb2pdf"
+  end
+
+  test do
+    out = shell_output("#{bin}/cb2pdf -v")
+    assert_match version.to_s, out
+  end
+end
