1e6134661701644215bbf65c177a39c1f6567110
Rémino Rem <29999+remino@users.noreply.github.com>
2026-05-12 20:53:23 +0900

    Update litesite to 1.3.0

--- Formula/litesite.rb
+++ Formula/litesite.rb
@@ -1,9 +1,9 @@
 # vim: set ft=ruby :
 class Litesite < Formula
   desc "Create and work with tiny static sites."
-  version "1.2.0"
-  url "https://github.com/remino/remutils/releases/download/litesite@1.2.0/litesite@1.2.0.tar.gz"
-  sha256 "0e9e60b5726ab6fdef8fba25cce21430ab6291a4327e11282eb245f484f202ec"
+  version "1.3.0"
+  url "https://github.com/remino/remutils/releases/download/litesite@1.3.0/litesite@1.3.0.tar.gz"
+  sha256 "4b2c882288e5f5d195ca00d0b031e45992fbe637e183baec65bfad78f7c57206"
   license "ISC"
   homepage "https://github.com/remino/remutils/tree/main/rsdeploy"
   revision 1
