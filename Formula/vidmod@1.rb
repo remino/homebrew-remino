100561c93a4bea97e53f64181e0c70805c12a773
Rémino Rem <29999+remino@users.noreply.github.com>
2026-07-07 18:04:44 +0900

    Move vidmod v1 formula to vidmod@1

---
+++ Formula/vidmod@1.rb
@@ -0,0 +1,20 @@
+class VidmodAT1 < Formula
+  desc "Do rapid changes to video files using ffmpeg or other tools"
+  homepage "https://github.com/remino/vidmod"
+  url "https://api.github.com/repos/remino/vidmod/tarball/v1.2.0"
+  sha256 "063e046e1a901af46c5f7ef42deba3f2818a4a594765aa88f4f85fcd70f4ebfb"
+  version "1.2.0"
+  license "ISC"
+
+  keg_only :versioned_formula
+
+  depends_on "ffmpeg"
+
+  def install
+    bin.install "vidmod"
+  end
+
+  test do
+    system bin/"vidmod", "-v"
+  end
+end
