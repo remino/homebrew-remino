bcd8b0b0fa19512fb083ee5c68b69fba62d814bf
Rémino Rem <29999+remino@users.noreply.github.com>
2026-07-07 17:35:35 +0900

    Update imgmod to 1.0.1

--- Formula/imgmod.rb
+++ Formula/imgmod.rb
@@ -1,9 +1,9 @@
 # vim: set ft=ruby :
 class Imgmod < Formula
   desc "Image modification helpers."
-  version "1.0.0"
-  url "https://github.com/remino/remutils/releases/download/imgmod@1.0.0/imgmod@1.0.0.tar.gz"
-  sha256 "51f8fe035b54f3b8aaf20c8629a0114ad01b1b844ce271eae5b5d66b32e6c0dd"
+  version "1.0.1"
+  url "https://github.com/remino/remutils/releases/download/imgmod@1.0.1/imgmod@1.0.1.tar.gz"
+  sha256 "9e25f596a36733d95a8fa76acc97f6db2c6f474732632b8ed515f51b2a609909"
   license "ISC"
   homepage "https://github.com/remino/remutils"
 
