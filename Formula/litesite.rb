7957dc9a5da11e8dd4892e8397ed3101fa9fd67c
Rémino Rem <29999+remino@users.noreply.github.com>
2026-07-10 21:32:49 +0900

    Update litesite to 2.0.0

--- Formula/litesite.rb
+++ Formula/litesite.rb
@@ -1,16 +1,15 @@
 # vim: set ft=ruby :
 class Litesite < Formula
   desc "Create and work with tiny static sites."
-  version "1.3.1"
-  url "https://github.com/remino/remutils/releases/download/litesite@1.3.1/litesite@1.3.1.tar.gz"
-  sha256 "d003618c9a4dbf506274e849578d876a5c5c5be6751a72db416cae4cc40950c1"
+  version "2.0.0"
+  url "https://github.com/remino/remutils/releases/download/litesite@2.0.0/litesite@2.0.0.tar.gz"
+  sha256 "2902946c34e39ef207201c47c4b62c7998c5bf617d2c93b1ee6f148a38abfd14"
   license "ISC"
-  homepage "https://github.com/remino/remutils/tree/main/rsdeploy"
+  homepage "https://github.com/remino/remutils/tree/main/litesite"
   revision 1
 
-  depends_on "bash"
+  depends_on "rust" => :build
   depends_on "imagemagick"
-  depends_on "node"
 
   livecheck do
     url :stable
@@ -18,9 +17,8 @@ class Litesite < Formula
   end
 
   def install
-    libexec.install Dir["*"]
-    bin.install_symlink libexec / "litesite" => "litesite"
-    man1.install libexec / "man/litesite.1"
+    system "cargo", "install", *std_cargo_args(path: ".")
+    man1.install "man/litesite.1"
   end
 
   test do
