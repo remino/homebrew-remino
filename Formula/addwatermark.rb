6e57262ce746535acef3f49b69040a49be203f89
Rémino Rem <29999+remino@users.noreply.github.com>
2026-08-02 18:31:46 +0900

    Update addwatermark to 2.1.1

--- Formula/addwatermark.rb
+++ Formula/addwatermark.rb
@@ -1,9 +1,9 @@
 # vim: set ft=ruby :
 class Addwatermark < Formula
   desc "Compatibility wrapper for imgmod watermark"
-  version "2.1.0"
-  url "https://github.com/remino/remutils/releases/download/addwatermark@2.1.0/addwatermark@2.1.0.tar.gz"
-  sha256 "55133054a6eaa6d8c5d18e3a050679573e683428d6f96a17431b4dd1ea3b0798"
+  version "2.1.1"
+  url "https://github.com/remino/remutils/releases/download/addwatermark@2.1.1/addwatermark@2.1.1.tar.gz"
+  sha256 "e2fad1757986eb01a75abf1411b1e395b24af8353c35cd41e68d2f6f35a3a43c"
   license "ISC"
   homepage "https://github.com/remino/remutils"
 
@@ -13,6 +13,7 @@ class Addwatermark < Formula
 
   def install
     bin.install "addwatermark"
+    man1.install "man/addwatermark.1"
   end
 
   test do
