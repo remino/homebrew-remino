559fddaf3f83ce74fe00e2d4f3c07cdebd97f8d0
Rémino Rem <29999+remino@users.noreply.github.com>
2026-08-02 18:57:08 +0900

    Update litesite to 2.3.1

--- Formula/litesite.rb
+++ Formula/litesite.rb
@@ -1,12 +1,11 @@
 # vim: set ft=ruby :
 class Litesite < Formula
   desc "Create and work with tiny static sites."
-  version "2.3.0"
-  url "https://github.com/remino/remutils/releases/download/litesite@2.3.0/litesite@2.3.0.tar.gz"
-  sha256 "d34c9603d0b8184d497545a4a980f45643953c3c2610542892ce3fecf70b0c45"
+  version "2.3.1"
+  url "https://github.com/remino/remutils/releases/download/litesite@2.3.1/litesite@2.3.1.tar.gz"
+  sha256 "7c522427a9bde1238b726205c8fe1706cb85591b3afa936e8a535983bf8b05bf"
   license "ISC"
   homepage "https://github.com/remino/remutils/tree/main/litesite"
-  revision 1
 
   depends_on "rust" => :build
   depends_on "imagemagick"
