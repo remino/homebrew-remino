26314d340aedc2a8209f8427767c24bacd408a7b
Rémino Rem <29999+remino@users.noreply.github.com>
2026-08-03 23:07:18 +0900

    Update mkprj to 3.0.2

--- Formula/mkprj.rb
+++ Formula/mkprj.rb
@@ -1,9 +1,9 @@
 # vim: set ft=ruby :
 class Mkprj < Formula
   desc "Create dated project directories from optional templates."
-  version "3.0.1"
-  url "https://github.com/remino/remutils/releases/download/mkprj@3.0.1/mkprj@3.0.1.tar.gz"
-  sha256 "4099180ffbddd752ef5a9d96e4986a0b7ce322c812e01674bba966cde80169f8"
+  version "3.0.2"
+  url "https://github.com/remino/remutils/releases/download/mkprj@3.0.2/mkprj@3.0.2.tar.gz"
+  sha256 "0f74480f8a0ab475520bffb856953d148e1a81f8b48c2dfbe8a82e2c567efa15"
   license "ISC"
   homepage "https://github.com/remino/remutils"
 
