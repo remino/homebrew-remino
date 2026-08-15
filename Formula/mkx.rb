9137356b6735ee66fc35c8da3239e790e6b24a18
Rémino Rem <29999+remino@users.noreply.github.com>
2026-08-15 12:10:57 +0900

    Update mkx to 3.3.0

--- Formula/mkx.rb
+++ Formula/mkx.rb
@@ -1,12 +1,11 @@
 # vim: set ft=ruby :
 class Mkx < Formula
   desc "Make new executable shell script file from template."
-  version "3.2.5"
-  url "https://github.com/remino/remutils/releases/download/mkx@3.2.5/mkx@3.2.5.tar.gz"
-  sha256 "e8cc3c0fb5fa9ac899155ade7b3a0d5cc33284fae89dcc6c102659b26abbb393"
+  version "3.3.0"
+  url "https://github.com/remino/remutils/releases/download/mkx@3.3.0/mkx@3.3.0.tar.gz"
+  sha256 "bc6f259fa0afcbc8768305b1790c0fb95aa6e93027b78c19d8a9785ffb165e63"
   license "ISC"
   homepage "https://github.com/remino/remutils"
-  revision 1
 
   depends_on "bash"
 
