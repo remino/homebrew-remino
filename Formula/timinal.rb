9a7406ae1d218df047680b5efca7b9f546e237b3
Rémino Rem <29999+remino@users.noreply.github.com>
2026-08-17 19:10:56 +0900

    Revert "Update termino to 0.2.0"
    This reverts commit d97c09a5f4a4e3f016bc716cdb3421402bee2a28.

--- Formula/timinal.rb
+++ Formula/timinal.rb
@@ -1,12 +1,15 @@
 # vim: set ft=ruby :
-class Termino < Formula
-  desc "Termino FIGlet font"
-  homepage "https://github.com/remino/termino"
-  url "https://github.com/remino/termino/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "4e49eb8ebf88cd4dc4cad74bc4246e0b80ed5b2259c4af97d81d187b67fa7bf8"
+class Timinal < Formula
+  desc "Render a configurable FIGlet clock"
+  version "0.1.0"
+  url "https://github.com/remino/remutils/releases/download/timinal@0.1.0/timinal@0.1.0.tar.gz"
+  sha256 "82128f849c06ab977e61b08024649c9c356bee34b0317d9a7835088800a64697"
   license "ISC"
+  homepage "https://github.com/remino/remutils/tree/main/timinal"
+  revision 1
 
   depends_on "figlet"
+  depends_on "python@3.14"
 
   livecheck do
     url :stable
@@ -14,29 +17,12 @@ class Termino < Formula
   end
 
   def install
-    (share/"figlet").install "termino.flf", "termino-raster.flf", "termino-tabular.flf", "termino-mono.flf"
-  end
-
-  def caveats
-    <<~EOS
-      To use Termino:
-        figlet -d #{opt_share}/figlet -f termino "TERMINO"
-
-      For the original # raster:
-        figlet -d #{opt_share}/figlet -f termino-raster "TERMINO"
-
-      For equal-width, centered numerals:
-        figlet -d #{opt_share}/figlet -f termino-tabular "2026"
-
-      For equal-width, centered characters:
-        figlet -d #{opt_share}/figlet -f termino-mono "TERMINO"
-    EOS
+    libexec.install "timinal.py"
+    (bin/"timinal").write_env_script libexec/"timinal.py", PATH: "#{Formula['python@3.14'].opt_bin}:$PATH"
+    man1.install "man/timinal.1"
   end
 
   test do
-    system Formula["figlet"].opt_bin/"figlet", "-d", share/"figlet", "-f", "termino", "TERMINO"
-    system Formula["figlet"].opt_bin/"figlet", "-d", share/"figlet", "-f", "termino-raster", "TERMINO"
-    system Formula["figlet"].opt_bin/"figlet", "-d", share/"figlet", "-f", "termino-tabular", "2026"
-    system Formula["figlet"].opt_bin/"figlet", "-d", share/"figlet", "-f", "termino-mono", "TERMINO"
+    system bin/"timinal", "--format", "%H:%M"
   end
 end
