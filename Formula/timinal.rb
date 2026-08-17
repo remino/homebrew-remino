d38e9483b51d0647516ad38940f460eeb6700ae1
Rémino Rem <29999+remino@users.noreply.github.com>
2026-08-17 18:47:53 +0900

    Add timinal 0.1.0

---
+++ Formula/timinal.rb
@@ -0,0 +1,27 @@
+# vim: set ft=ruby :
+class Timinal < Formula
+  desc "Render a configurable FIGlet clock"
+  version "0.1.0"
+  url "https://github.com/remino/remutils/releases/download/timinal@0.1.0/timinal@0.1.0.tar.gz"
+  sha256 "82128f849c06ab977e61b08024649c9c356bee34b0317d9a7835088800a64697"
+  license "ISC"
+  homepage "https://github.com/remino/remutils/tree/main/timinal"
+
+  depends_on "figlet"
+  depends_on "python@3.14"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  def install
+    libexec.install "timinal.py"
+    (bin/"timinal").write_env_script libexec/"timinal.py"
+    man1.install "man/timinal.1"
+  end
+
+  test do
+    system bin/"timinal", "--format", "%H:%M"
+  end
+end
