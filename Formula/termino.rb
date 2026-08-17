20a0ffef0ea455155e94a450edd40e6c8154c31a
Rémino Rem <29999+remino@users.noreply.github.com>
2026-08-17 12:07:18 +0900

    Update termino to 0.1.0_3

--- Formula/termino.rb
+++ Formula/termino.rb
@@ -4,11 +4,12 @@ class Termino < Formula
   homepage "https://github.com/remino/termino"
   url "https://github.com/remino/termino/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "2692a01af866d2de226211ec0dc1c7155f6a6a65e4b79e80b066f9ab65ac07a7"
-  revision 2
+  revision 3
   license "ISC"
 
   depends_on "figlet"
-  depends_on "python@3.14" => :optional
+  option "with-clock", "Install termino-clock (requires Python)"
+  depends_on "python@3.14" if build.with? "clock"
 
   livecheck do
     url :stable
@@ -17,7 +18,7 @@ class Termino < Formula
 
   def install
     (share/"figlet").install "termino.flf", "termino-tabular.flf", "termino-mono.flf"
-    if build.with? "python@3.14"
+    if build.with? "clock"
       libexec.install "bin/termino-clock"
       (bin/"termino-clock").write_env_script libexec/"termino-clock", PATH: "#{Formula['python@3.14'].opt_bin}:#{Formula['figlet'].opt_bin}:$PATH"
       man1.install "man/termino-clock.1"
@@ -27,13 +28,13 @@ class Termino < Formula
   def caveats
     <<~EOS
       To use Termino:
-        figlet -f termino "TERMINO"
+        figlet -d #{opt_share}/figlet -f termino "TERMINO"
 
       For equal-width, centered numerals:
-        figlet -f termino-tabular "2026"
+        figlet -d #{opt_share}/figlet -f termino-tabular "2026"
 
       For equal-width, centered characters:
-        figlet -f termino-mono "TERMINO"
+        figlet -d #{opt_share}/figlet -f termino-mono "TERMINO"
     EOS
   end
 
@@ -41,9 +42,6 @@ class Termino < Formula
     system Formula["figlet"].opt_bin/"figlet", "-d", share/"figlet", "-f", "termino", "TERMINO"
     system Formula["figlet"].opt_bin/"figlet", "-d", share/"figlet", "-f", "termino-tabular", "2026"
     system Formula["figlet"].opt_bin/"figlet", "-d", share/"figlet", "-f", "termino-mono", "TERMINO"
-    system Formula["figlet"].opt_bin/"figlet", "-f", "termino", "TERMINO"
-    system Formula["figlet"].opt_bin/"figlet", "-f", "termino-tabular", "2026"
-    system Formula["figlet"].opt_bin/"figlet", "-f", "termino-mono", "TERMINO"
-    system bin/"termino-clock", "--time-only" if build.with? "python@3.14"
+    system bin/"termino-clock", "--time-only" if build.with? "clock"
   end
 end
