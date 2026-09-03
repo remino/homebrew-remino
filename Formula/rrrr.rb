3f7d17632f3c7a74e40da68a1102c2ecafc4918b
Rémino Rem <29999+remino@users.noreply.github.com>
2026-09-03 22:03:39 +0900

    Update rrrr to 2.0.0

--- Formula/rrrr.rb
+++ Formula/rrrr.rb
@@ -1,9 +1,9 @@
 # vim: set ft=ruby :
 class Rrrr < Formula
   desc "Create rsync snapshot backups over SSH"
-  version "1.3.0"
-  url "https://github.com/remino/remutils/releases/download/rrrr@1.3.0/rrrr@1.3.0.tar.gz"
-  sha256 "4f2eb5ef36276a938932289719670c3a788b98ae223ffa06bf8560bc4c6f459e"
+  version "2.0.0"
+  url "https://github.com/remino/remutils/releases/download/rrrr@2.0.0/rrrr@2.0.0.tar.gz"
+  sha256 "853795905d7adeee24bc4cc6c3da5e90c9a737a72b26763aee42c0dabcf2dabb"
   license "ISC"
   homepage "https://github.com/remino/remutils/tree/main/rrrr"
 
