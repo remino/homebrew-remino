0c902557455323be6ef7b716e2e89f07b0f683c3
Rémino Rem <29999+remino@users.noreply.github.com>
2026-09-23 16:41:47 +0900

    Move maconf to remutils

--- Formula/maconf.rb
+++ Formula/maconf.rb
@@ -1,16 +1,25 @@
+# vim: set ft=ruby :
 class Maconf < Formula
-	desc "Get and set various settings in macOS"
-	homepage "https://github.com/remino/maconf"
-	url "https://api.github.com/repos/remino/maconf/tarball/v1.1.1"
-	sha256 "c0c0a708764ca93cd3f88064b2692dd39f6606b2408aada1019f49e450003c1b"
-	version "1.1.1"
-	license "ISC"
+  desc "Get and set common macOS preferences"
+  url "https://github.com/remino/remutils/releases/download/maconf@1.1.1/maconf@1.1.1.tar.gz"
+  sha256 "560100a79f85fdca05f94d56f81b2288319aed6e92332c77b20247a3eda95a02"
+  license "ISC"
+  homepage "https://github.com/remino/remutils/tree/main/maconf"
+  revision 1
 
-	def install
-		bin.install "./maconf"
-	end
+  depends_on :macos
 
-	test do
-		system "./maconf", "-v"
-	end
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  def install
+    bin.install "maconf"
+    man1.install "man/maconf.1"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/maconf -v")
+  end
 end
