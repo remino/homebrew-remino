94828b7db1eb5778468d721bb2e0754dfa44e57e
Rémino Rem <29999+remino@users.noreply.github.com>
2026-09-26 20:18:57 +0900

    Fix iftttnotify release checksum

--- Formula/iftttnotify.rb
+++ Formula/iftttnotify.rb
@@ -1,11 +1,11 @@
 # vim: set ft=ruby :
 class Iftttnotify < Formula
-  desc "Send IFTTT Webhooks notifications from the command line"
-  url "https://github.com/remino/remutils/releases/download/iftttnotify@1.0.1/iftttnotify@1.0.1.tar.gz"
-  sha256 "e08931312be8c40d6d575fd17ad456ef535e2b1401cbc4b1fb06d83b2ab6d03d"
-  license "ISC"
+  desc "Send IFTTT Webhooks notifications from the command-line"
   homepage "https://github.com/remino/remutils/tree/main/iftttnotify"
-  revision 1
+  url "https://github.com/remino/remutils/releases/download/iftttnotify@1.0.1/iftttnotify@1.0.1.tar.gz"
+  sha256 "ff29a845a820e9fd518942c2cd13d034825b59bc171f180b659f9e2b52c4eee1"
+  license "ISC"
+  revision 2
 
   livecheck do
     url :stable
